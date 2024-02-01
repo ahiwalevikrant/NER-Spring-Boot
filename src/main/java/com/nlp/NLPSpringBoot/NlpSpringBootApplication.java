14d90440409c624016a1164aba700f22f0a4792e
ahiwalevikrant <vikrantahiwale71@gmail.com>
2024-02-01 06:38:59 -0500

    NER api Implementation

---
+++ src/main/java/com/nlp/NLPSpringBoot/NlpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nlp.NLPSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nlp/NLPSpringBoot/NlpSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlp.NLPSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
